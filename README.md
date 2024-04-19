fb3e91d3b687426ec0d076572bf552fcd207fd5b
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-04-19 10:42:33 -0300

    feat: Modificar función para que devuelva DataFrame con estadísticas

---
+++ README.md
@@ -0,0 +1,28 @@
+# Análisis Estadístico de Valores Numéricos
+
+Este script de Python realiza un análisis estadístico básico en una lista de valores numéricos. Calcula varias métricas estadísticas que incluyen frecuencia absoluta, frecuencia acumulada, frecuencia relativa, frecuencia relativa acumulada, probabilidad y probabilidad acumulada en porcentaje.
+## Funcionalidades
+
+- **Frecuencia Absoluta (`fi`)**: Cuenta la ocurrencia de cada valor en la lista.
+  
+- **Frecuencia Acumulada (`Fi`)**: Calcula la suma acumulada de las frecuencias absolutas.
+
+- **Frecuencia Relativa (`ri`)**: Calcula la proporción de cada valor en relación al tamaño total de la muestra.
+
+- **Frecuencia Relativa Acumulada (`Ri`)**: Calcula la suma acumulada de las frecuencias relativas.
+
+- **Probabilidad (`pi%`)**: Calcula la probabilidad de cada valor en porcentaje.
+
+- **Probabilidad Acumulada (`Pi%`)**: Calcula la suma acumulada de las probabilidades en porcentaje.
+
+## Requisitos
+
+- Python 3.x
+- Biblioteca pandas
+
+## Uso
+
+1. **Clonar el Repositorio:**
+
+   ```bash
+   git clone https://github.com/Leonardo0294/python-statistical-analysis.git
--- analisis_estadistico.py
+++ analisis_estadistico.py
@@ -1,66 +1,57 @@
 import pandas as pd
 
-def analisis_estadistico(valores):
+# Lista de edades de estudiantes
+edades_estudiantes = [34, 25, 19, 36, 36, 34, 32, 36, 34, 29,
+                      21, 36, 24, 35, 29, 24, 30, 34, 24, 25,
+                      31, 19, 24, 26, 26, 30, 35, 21, 31, 1]
+
+def analisis_estadistico(data: list[float | int]) -> pd.DataFrame:
     """
-    Realiza un análisis estadístico básico de una lista de valores numéricos.
+    Toma una lista de valores numéricos y calcula estadísticas descriptivas.
+    
+    Args:
+        data (list[float | int]): Lista de valores numéricos.
 
-    Parámetros:
-    valores (list): Lista de valores numéricos.
-
-    Retorna:
-    pandas.DataFrame: Dataframe que contiene las siguientes columnas como claves:
-        - 'fi': Frecuencia absoluta de cada valor en la lista.
-        - 'Fi': Frecuencia acumulada.
-        - 'ri': Frecuencia relativa.
-        - 'Ri': Frecuencia relativa acumulada.
-        - 'pi%': Probabilidad (en porcentaje).
-        - 'Pi%': Probabilidad acumulada (en porcentaje).
+    Returns:
+        pd.DataFrame: DataFrame con las estadísticas calculadas.
+            - x: Valores únicos de la lista.
+            - fi: Frecuencia absoluta de cada valor.
+            - Fi: Frecuencia acumulada.
+            - ri: Frecuencia relativa.
+            - Ri: Frecuencia relativa acumulada.
+            - pi: Frecuencia porcentual.
+            - Pi: Frecuencia porcentual acumulada.
     """
-    # Se calcula la frecuencia absoluta (fi) de cada valor
-    fi_series = pd.Series(valores).value_counts().sort_index()
-    fi = fi_series.to_dict()
+    # Se valida que los datos sean una lista no vacía
+    if not data or not isinstance(data, list):
+        raise ValueError("Los datos deben estar dispuestos en una lista.")
 
-    # Se calcula la frecuencia acumulada (Fi)
-    Fi = pd.Series(fi).cumsum()
+    # Se convierte la lista de datos en un DataFrame de pandas
+    df = pd.DataFrame(data, columns=["edad"])
 
-    # Se calcula el tamaño total de la muestra (n)
-    n = len(valores)
+    # Se calcula la frecuencia absoluta (fi) de cada valor único
+    data_frame = df["edad"].value_counts().sort_index().reset_index()
+    data_frame.columns = ["x", "fi"]
 
-    # Se calcula la frecuencia relativa (ri)
-    ri = {valor: frecuencia / n for valor, frecuencia in fi.items()}
+    # Se calculan las frecuencias acumuladas (Fi), relativas (ri, Ri) y porcentuales (pi, Pi)
+    data_frame["Fi"] = data_frame["fi"].cumsum()
+    data_frame["ri"] = data_frame["fi"] / len(data) 
+    data_frame["Ri"] = data_frame["ri"].cumsum()
+    data_frame["pi"] = data_frame["ri"] * 100
+    data_frame["Pi"] = data_frame["Ri"] * 100
 
-    # Se calcula la frecuencia relativa acumulada (Ri)
-    Ri = pd.Series(ri).cumsum()
+    return data_frame
 
-    # Se calcula la probabilidad (pi) en porcentaje
-    pi_percent = {valor: frecuencia / n * 100 for valor, frecuencia in fi.items()}
+def main():
+    # Se realizael análisis estadístico de las edades (edades_estudiantes)
+    resultado_analisis = analisis_estadistico(edades_estudiantes)
 
-    # Se calcula la probabilidad acumulada (Pi) en porcentaje
-    Pi_percent = pd.Series(pi_percent).cumsum()
+    # Se imprime el resultado del análisis estadístico
+    print("Análisis Estadístico de Edades:")
+    print(resultado_analisis)
 
-    # Se crea el dataframe con las estadísticas calculadas
-    df_estadisticas = pd.DataFrame({
-        'fi': list(fi.values()),
-        'Fi': Fi,
-        'ri': list(ri.values()),
-        'Ri': Ri,
-        'pi%': list(pi_percent.values()),
-        'Pi%': Pi_percent
-    }, index=list(fi.keys()))
+    # El resultado se copia al portapapeles
+    resultado_analisis.to_clipboard(index=False)
 
-    # Se formatea el dataframe para mostrar dos decimales en los resultados
-    pd.options.display.float_format = '{:,.2f}'.format
-
-    # Se copia el dataframe al portapapeles y se muestra por consola
-    df_estadisticas.to_clipboard()
-    print(df_estadisticas)
-
-    return df_estadisticas
-
-# Valores
-valores = [34, 25, 19, 36, 36, 34, 32, 36, 34, 29,
-           21, 36, 24, 35, 29, 24, 30, 34, 24, 25,
-           31, 19, 24, 26, 26, 30, 35, 21, 31, 19]
-
-# análisis estadístico de los valores
-resultado_analisis = analisis_estadistico(valores)
+if __name__ == "__main__":
+    main()
